b3e27ae6fe1a482907bd131e005b1589e1aa97ed
Kyle <kdevos827@gmail.com>
2023-09-07 00:03:29 +0200

    Added Docstrings and Comments

--- proj/django_capstone/crafts_by_micks/models.py
+++ proj/django_capstone/crafts_by_micks/models.py
@@ -1,25 +1,96 @@
+"""Models for Django App 'crafts_by_micks' for Product, Order and associated Attributes Creation
+
+Classes:
+--------
+Label
+Category
+Product
+Option
+ProductSizes
+Review
+"""
 from django.db import models
 
-# Create your models here.
-
 class Label(models.Model):
+    """Class modeling a Product Label ('new', 'discount', etc). Product to have Many-To-One
+        Relationship allowing one Product to have many Labels.
+    Child class of django models.Model.
+
+    Attributes:
+    -----------
+    title: str (CharField)
+        title (name) of label
+    discount_percentage: int (IntegerField)
+        percentage of discount to be applied to a product (applied to all sizes)
+    creation_date: DateField()
+        Date of Product Creation
+    removal_days: int (IntegerField)
+        optional number of days after which label must be removed from a product
+    custom_colour: str (Charfield)
+        user chosen colour (default light blue) for label background
+
+    Methods:
+    --------
+    __str__(self): str
+        return title of label
+    """
     title = models.CharField(max_length=20)
     discount_percentage = models.IntegerField(null=True)
     creation_date = models.DateField()
     removal_days = models.IntegerField(null=True)
-    custom_colour = models.CharField(max_length=10, null = True, default="#89CFF0") # default light blue
+    # default colour set to 'light blue'
+    custom_colour = models.CharField(max_length=10, null = True, default="#89CFF0")
 
     def __str__(self):
+        """Return title of Label"""
         return self.title
 
+
 class Category(models.Model):
+    """Class modeling a Product Category. Each Product should belong to one Category, however
+        Many-To-One Relationship set instead for possible future changes. 'create_product.html'
+        currently set to maintain One-To-One Relationship. Child class of django models.Model.
+
+    Attributes:
+    -----------
+    title: str (CharField)
+        title (name) of Category
+
+    Methods:
+    --------
+    __str__(self): str
+        return title of Category
+    """
     title = models.CharField(max_length=50)
 
     def __str__(self):
+        "Return Title of Category"
         return self.title
 
 
 class Product(models.Model):
+    """Class modeling a Product . Size Variations are handled within Product and should
+        not form part of name. Child class of django models.Model.
+
+    Attributes:
+    -----------
+    title - str (CharField)
+        title (name) of Product
+    description: str (TextField):
+        A description of the current Product
+    review_vaue: int (IntegerField)
+        review rating of product - default value set to 0. Functionality allowing
+        admin (not superuser) to modify review in any way has not been added.
+        percentage of discount to be applied to a product (applied to all sizes)
+    labels: Label 
+        Associated Label objects for Product
+    category: Category
+        Associated Category for Product
+    Methods:
+    --------
+    __str__(self): str
+        return title of Product
+    """
     title = models.CharField(max_length=200)
     description = models.TextField()
     review_value = 0.0
@@ -29,19 +100,59 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
 
     def __str__(self):
+        """Return Title of Product"""
         return self.title
 
 
 class Option(models.Model):
+    """Class modeling Options for a Product.
+        An Option is consider as any design variation or detailed product execution
+        such as a style change, scented, material variatoion, etc. Cascading Deletion with
+        associated Product.
+
+    Attributes:
+    -----------
+    product: Product 
+        Associated Product Object
+    title: str (CharField)
+        Name of Product
+    description: str (Charfield)
+        A short description giving detail of the Option
+    Methods:
+    --------
+    __str__(self): str
+        return Associated product id and title of Option
+    """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100, null=True)
 
     def __str__(self):
+        """return Associated product id and title of Option"""
         return f"Product: {self.product.id}, {self.title}"
     
 
 class Product_Sizes(models.Model):
+    """Class modeling Size_Options for a Product with corresponding price.
+        Products with no size option take size as 'none'. Cascading Deletion
+        from associated Product.
+
+    Attributes:
+    -----------
+    product: Product 
+        Associated Product Object
+    size_options: tupple
+        contains tuples for each product size
+    size: str (Charfield)
+        selected size for Product
+    price: float
+        price for Product according to size
+
+    Methods:
+    --------
+    __str__(self): str
+        return Product id and size
+    """
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
     size_options = (
         ('n', 'none'),
@@ -58,16 +169,34 @@ class Product_Sizes(models.Model):
     quantity = models.IntegerField(default=0)
 
     def __str__(self):
+        """return Product id and size"""
         return f"Product: {self.product.id} {self.size}"
 
 
-
 class Review(models.Model):
+    """Class modeling a Review for a Product.
+
+    Attributes:
+    -----------
+    product: Product 
+        Associated Product Object
+    author: str (Charfield)
+        name of person writing review
+    description: str (TextField)
+        review content
+    rating: int (IntegerField)
+        review rating
+    Methods:
+    --------
+    __str__(self): str
+        return author and id of review
+    """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     author = models.TextField(max_length=200)
     description = models.TextField(null=True)
     rating = models.IntegerField()
 
     def __str__(self):
+        """ return author and id of review"""
         return f"{self.author}, Review No: {self.id}"
     
--- proj/django_capstone/crafts_by_micks/urls.py
+++ proj/django_capstone/crafts_by_micks/urls.py
@@ -1,13 +1,23 @@
+"""URL paths for app: 'crafts_by_micks' within Django project 'django_capstone.
+    Includes Path for site admin control and user usage.'"""
+
 from django.urls import path
 from . import views
 
 app_name = "crafts_by_micks"
 urlpatterns = [
+    # Home Page of App showing user signup, login, product catalog and orders
     path('', views.home_page, name = "home_page"),
+    # admin control - create a new Product Category
     path('<source>/create_category/', views.create_category, name = "create_category"),
+    # admin control - add newly created Product Category to Database
     path('<source>/add_category/', views.add_category, name = 'add_category'),
+    # admin control - create a new Product Label
     path('<source>/create_label/', views.create_label, name='create_label'),
+    # admin control - add newly created label to database
     path('<source>/add_label', views.add_label, name="add_label"),
+    # admin control - Create a New Product
     path('create_product/', views.create_product, name="create_product"),
+    # admin control - add newly created product to database
     path('add_product/', views.add_product, name="add_product")
 ]
