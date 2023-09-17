c02b54725a8f19228b741158ead314808a2f7597
Kyle <kdevos827@gmail.com>
2023-09-17 09:29:15 +0200

    Basic Structure for login page - non functional

--- proj/django_capstone/user_management/views.py
+++ proj/django_capstone/user_management/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def user_login(request):
-    return HttpResponse("Login Page")
+    return render(request, 'user_login.html')
