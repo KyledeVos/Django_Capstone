30431378a1d95b9c487fadbb83bc74cd9ed48db5
Kyle <kdevos827@gmail.com>
2023-09-17 14:15:18 +0200

    User Login - Check for spaces as input

--- proj/django_capstone/user_management/urls.py
+++ proj/django_capstone/user_management/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name='user_management'
 urlpatterns = [
     # user login
-    path('user_login/', views.user_login, name='user_login'),
+    path('<error>/user_login/', views.user_login, name='user_login'),
+    # authenticate user - attempt login
+    path('authenticate_user/', views.authenticate_user, name='authenticate_user'),
     # user signup
     path('create_user/', views.create_user, name='create_user')
 ]
--- proj/django_capstone/user_management/views.py
+++ proj/django_capstone/user_management/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
-def user_login(request):
-    return render(request, 'user_login.html')
+def user_login(request, error = 'none'):
+    return render(request, 'user_login.html', {'error': error})
 
 
 def create_user(request):
     return render(request, 'create_user.html')
+
+def authenticate_user(request):
+
+
+    # retrieve username and password from login attempt
+    username= request.POST['username']
+    password = request.POST['password']
+
+    # check username and password do not contain empty characters
+    if username.isspace() or password.isspace():
+        error_message = "Login Failed - Username and\\or password did not contain any characters"
+
+        return HttpResponseRedirect(reverse('user_management:user_login', args=(error_message,)))
+
+
+    return HttpResponse(f"Name: {username}, Password: {password}")
