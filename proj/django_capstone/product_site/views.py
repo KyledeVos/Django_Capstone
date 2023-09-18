c1edab5da79f09acc00987f6249efcefc9e3afd6
Kyle <kdevos827@gmail.com>
2023-09-18 08:38:12 +0200

    Completed User Login - page return logic not completed

--- proj/django_capstone/product_site/views.py
+++ proj/django_capstone/product_site/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
+def logincheck(request):
+    """Determine if a current user is authenticated
+
+    Parameter:
+    -----------
+    request: HTTPRequest object
+        contains metadata about the request needed for html page render
+
+    Return:
+    -------
+    True if user is authenticated, False if not
+    """
+    if request.user.is_authenticated:
+        return True
+    else: 
+        return False
+
 def site_home(request):
-    return render(request, 'site_home.html')
+    context = {
+        'logged_in': logincheck(request),
+        'username': request.user.username
+    }
+    return render(request, 'site_home.html', context)
--- proj/django_capstone/user_management/views.py
+++ proj/django_capstone/user_management/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-
-# Create your views here.
-
-def user_login(request, error = 'none'):
-    return render(request, 'user_login.html', {'error': error})
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def create_user(request):
     return render(request, 'create_user.html')
 
-def authenticate_user(request):
 
+def user_login(request, error = 'none'):
+    return render(request, 'user_login.html', {'error': error})
+
+
+
+def authenticate_user(request):
 
     # retrieve username and password from login attempt
     username= request.POST['username']
@@ -20,8 +22,16 @@ def authenticate_user(request):
     # check username and password do not contain empty characters
     if username.isspace() or password.isspace():
         error_message = "Login Failed - Username and\\or password did not contain any characters"
-
         return HttpResponseRedirect(reverse('user_management:user_login', args=(error_message,)))
 
+    # attempt to authenticate user
+    user = authenticate(username=username, password = password)
 
-    return HttpResponse(f"Name: {username}, Password: {password}")
+    if user is None:
+        # unsuccessful authentication
+        error_message = "Invalid username and\\or password - Please Try again"
+        # allow user to try login again
+        return HttpResponseRedirect(reverse('user_management:user_login', args=(error_message,)))
+    else:
+        # successful login
+        return HttpResponse(f"Name: {username}, Password: {password}")
