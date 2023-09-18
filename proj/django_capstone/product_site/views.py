e496c0186b56c7ada440700b0e23db80087443f7
Kyle <kdevos827@gmail.com>
2023-09-19 00:00:55 +0200

    Docstrings Added

--- proj/django_capstone/product_site/views.py
+++ proj/django_capstone/product_site/views.py
@@ -1,24 +1,31 @@
+"""Views Module for Django App 'product_site'. App serves as central site
+for web application.
+
+Methods:
+--------
+logincheck(request):
+    Determine if a current user is authenticated
+
+site_home(request):
+    Render main site home page, checking if currently logged-in user is a staff
+        member allowing access to admin control functionality
+"""
 from django.shortcuts import render, HttpResponse
 
 def logincheck(request):
-    """Determine if a current user is authenticated
+    """Determine if a current user is authenticated. """
+    return True if request.user.is_authenticated else False
 
-    Parameter:
-    -----------
-    request: HTTPRequest object
-        contains metadata about the request needed for html page render
-
-    Return:
-    -------
-    True if user is authenticated, False if not
-    """
-    if request.user.is_authenticated:
-        return True
-    else: 
-        return False
 
 def site_home(request):
-
+    """Render main site home page, checking if currently logged-in user is a staff
+        member allowing access to admin control functionality.
+         
+    Parameter:
+    ----------
+    request: HTTPRequest object
+        required for user authentication, status check and html page render
+    """
     # Determine if the user has been logged in
     logged_in = logincheck(request)
     # if the user has been checked in, check if they are staff; giving access to admin control
