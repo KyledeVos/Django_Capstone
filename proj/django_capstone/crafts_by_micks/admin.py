44327d0522012c74185916677957bed540e857ed
Kyle <kdevos827@gmail.com>
2023-09-22 12:18:16 +0200

    Registered Order model to Admin Site

--- proj/django_capstone/crafts_by_micks/admin.py
+++ proj/django_capstone/crafts_by_micks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Product_Images, Label, Category, Review, Product_Sizes, Option, Order_Item
+from .models import Product, Product_Images, Label, Category, Review, Product_Sizes, Option, Order_Item, Order
 
 # Register your models here.
 admin.site.register(Product)
@@ -10,4 +10,5 @@ admin.site.register(Review)
 admin.site.register(Product_Sizes)
 admin.site.register(Option)
 admin.site.register(Order_Item)
+admin.site.register(Order)
 
