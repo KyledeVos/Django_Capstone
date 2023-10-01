a9ec6f6b1b4fcefe4e5e71c3db1883f12020cc01
Kyle <kdevos827@gmail.com>
2023-10-01 18:03:54 +0200

    Code Cleanup

--- proj/django_capstone/crafts_by_micks/admin.py
+++ proj/django_capstone/crafts_by_micks/admin.py
@@ -1,7 +1,7 @@
+# Models to be loaded to Django Admin Site for superuser control
 from django.contrib import admin
 from .models import Product, Product_Images, Label, Category, Review, Product_Sizes, Option, Order_Item, Order
 
-# Register your models here.
 admin.site.register(Product)
 admin.site.register(Product_Images)
 admin.site.register(Label)
@@ -11,4 +11,3 @@ admin.site.register(Product_Sizes)
 admin.site.register(Option)
 admin.site.register(Order_Item)
 admin.site.register(Order)
-
